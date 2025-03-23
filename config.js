089115524d52e3df80c22ded5515508842bcdbf4
Lemon <phillipow2@gmail.com>
2025-03-22 22:08:14 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,29 @@
+const gameConfig = {
+    initialCoins: 1000,
+    initialLevel: 1,
+    
+    bazarAuctionItems: 8,
+    bazarTradeItems: 6,
+    
+    marketItems: 12,
+    
+    pigsInventoryItems: 8,
+    itemsInventoryItems: 16,
+    
+    statsItems: 8,
+    profileItems: 4,
+    settingsOptions: 6,
+    
+    creditsItems: 4,
+    
+    colors: {
+        primary: '#FF90B3',
+        secondary: '#ff4081',
+        background: '#f9f9f9',
+        container: '#f0f0f0',
+        text: '#333'
+    }
+};
+
+window.gameConfig = gameConfig;
+
